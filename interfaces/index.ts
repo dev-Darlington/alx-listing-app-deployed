b9c4fb6404c4f844178828e6e2d12507c86207dc
Darlington <darlingtonuchenna53@gmail.com>
2025-11-16 23:39:56 +0100

    updates

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,18 @@
-const index = {}
+interface PropertyProps {
+    name: string;
+    address: {
+        state: string;
+        city: string;
+        country: string;
+    };
+    rating: number;
+    category: string[];
+    price: number;
+    offers: {
+        bed: string;
+        shower: string;
+        occupants: string;
+    };
+    image: string;
+    discount: string;
+}
